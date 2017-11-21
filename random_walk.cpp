ccd0ace3d357ebd6aced214e12334ee62da3e846
Alexander Swenson <aaswenson@wisc.edu>
2017-11-21 10:11:24 -0600

    bug fix in random walk, expanded event_log class. The class has all required methods to track particle. Including example event history. Also added a normal dist. to sample energy

--- random_walk.cpp
+++ random_walk.cpp
@@ -4,26 +4,61 @@
 #include <vector>
 #include <fstream>
 #include <iomanip>
+#include <random>
 
 #define SPHERE_RADIUS 1000
 #define MEAN_FREE_PATH 10
+#define SEED 123
+
+std::vector<double> sample_direction();
+double sample_path_length();
 
 class event_log {
+    // This class stores information about particle collision events. It also
+    // updates particle radius and location based on sampled collision data.
+
     public:
-// store path length
-    double pl;
-//  store collision location
-    double x = 0;
-    double y = 0;
-    double z = 0;
-// calculate particle radius
-    double radius() {return sqrt(x*x + y*y + z*z);}
-// store particle direction
-    double u;
-    double v;
-    double w;
-// store particle energy
-    double E;
+    // store path length
+        double pl;
+    //  collision location
+        double x = 0; double y = 0; double z = 0;
+    // particle direction
+        double u; double v; double w;
+    // store particle energy
+        double E;
+
+    // Get particle distance
+        void get_distance(){ pl = sample_path_length();}
+    // Get particle direction
+        void get_direction(){
+            std::vector<double> direction = sample_direction();
+            u = direction[0];
+            v = direction[1];
+            w = direction[2];
+        }
+    // Get particle energy
+        void get_energy(){ 
+            // set normal dist for E sampling
+            std::default_random_engine generator;
+            std::normal_distribution<double> normal_dist(5.0,2.0);
+            E = normal_dist(generator);
+        }
+    // calculate particle radius
+        double radius() {return sqrt(x*x + y*y + z*z);}
+    // update particle location
+        void update_location() {
+            x += pl*u;
+            y += pl*v;
+            z += pl*w;
+        }
+
+    // save particle log
+        void save_log(std::ofstream& logfile){
+        logfile << std::left << x << "     " << y << "     " << z << "     ";
+        logfile << std::left << u << "     " << v << "     " << w << "     ";
+        logfile << std::left << pl << "     ";
+        logfile << std::left << E << "\n";
+        }
 };
 
 
@@ -56,29 +91,23 @@ std::vector<double> sample_direction(){
 void walk_particle(std::ofstream& logfile){
     // intialize particle
     event_log collision;
-    
+    // set initial energy, distance, direction
+    collision.get_energy();
+    collision.get_distance();
+    collision.get_direction();
+    // save initial particle pos, energy
+    collision.save_log(logfile);
+    // update location for next interaction
+    collision.update_location();
+
     while (collision.radius() < SPHERE_RADIUS){
-        // sample path length
-        collision.pl = sample_path_length();
 
-        // Get particle direction
-        std::vector<double> direction = sample_direction();
-        collision.u = direction[0];
-        collision.v = direction[1];
-        collision.w = direction[2];
-
-        // Get new particle location
-        collision.x += collision.u * collision.pl;
-        collision.y += collision.v * collision.pl;
-        collision.z += collision.w * collision.pl;
-
-        // write the event history to the log file
-        logfile << std::left << collision.x << "     " << 
-                   collision.y << "     " << 
-                   collision.z << "     ";
-        logfile << std::left << collision.u << "     " << 
-                   collision.v << "     " << 
-                   collision.w << "     \n";
+        // update info, save and calculate new location
+        collision.get_energy();
+        collision.get_distance();
+        collision.get_direction();
+        collision.save_log(logfile);
+        collision.update_location();
     }
 
 }
@@ -91,9 +120,9 @@ int main(int argc, char** argv){
     }
     int N = atoi(argv[1]);
     int i;
-
+    srand(SEED);
     std::ofstream event_history;
-    event_history.open("event_history.txt", std::ofstream::app);
+    event_history.open("event_history.txt");
     
     for (i=0; i < N; i++){
         walk_particle(event_history);
