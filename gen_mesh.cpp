095e4f5a11a319174b660f8bdb4b8c274e9fd283
Alexander Swenson <aaswenson@wisc.edu>
2017-12-11 20:06:31 -0600

    continuing to clean up the gen_mesh.cpp file

--- gen_mesh.cpp
+++ gen_mesh.cpp
@@ -4,97 +4,77 @@
 
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
+#include <string>
 
 using namespace moab;
 
 int main(int argc, char* argv[])
 {
-  moab::Core mbcore;
-  moab::Interface& mbint = mbcore;
-
-  // ***********************
-  // *   Create the Mesh   *
-  // ***********************
-
-  // First, lets make the mesh. It will be a 100 by 100 uniform grid
-  // (there will be 100x100 quads, 101x101 vertexes) with dx = dy =
-  // 0.1. Unlike the previous example, we will first make the mesh,
-  // then set the coordinates one at a time.
-
-  const unsigned NI = atoi(argv[1]);
-  const unsigned NJ = atoi(argv[2]);
-  const unsigned NK = atoi(argv[3]);
-
-  // moab::ScdInterface, structured mesh interface for MOAB
-  moab::ScdInterface *scdint;
-
-  // Tell MOAB that our mesh is structured:
-  moab::ErrorCode rval = mbint.query_interface(scdint);
-  MB_CHK_SET_ERR(rval, "mbint.query_interface");
-  
-
-  // Create the mesh:
-  moab::ScdBox *scdbox = NULL;
-  rval = scdint->construct_box(moab::HomCoord(0,0,0), 
-			       moab::HomCoord(NI,NJ,NK),
-			       NULL, 
-			       0, 
-			       scdbox);
-  MB_CHK_SET_ERR(rval, "scdint->construct_box");
-
-
-  // ******************************
-  // *   Set Vertex Coordinates   *
-  // ******************************
-  
-  const double DX = 0.1;
-  const double DY = 0.1;
-  const double DZ = 0.1;
-
-  for(unsigned i = 0; i < NI+1; i++) 
-    for(unsigned j = 0; j < NJ+1; j++)
-        for(unsigned k = 0; k < NK+1; k++) {
-      // First, get the entity handle:
-      moab::EntityHandle handle = scdbox->get_vertex(i,j,k);
-
-      // Compute the coordinate:
-      double coord[3] = {DX*i, DY*j, DZ*k};
-
-      // Change the coordinate of the vertex:
-      mbint.set_coords(&handle, 1, coord);
+    if (argc != 7){
+        std::cout << "Usage: Nx Ny Nz hx hy hz" << std::endl;
+        return 1;
     }
 
+    std::string N = (1, argv[1]);
+    std::string h = (1, argv[2]);
+    std::string mesh_name = "structured_mesh_" + N + "_" + h + ".vtk";
+    const unsigned NI = atoi(argv[1]);
+    const unsigned NJ = atoi(argv[2]);
+    const unsigned NK = atoi(argv[3]);
+    const double DX = atoi(argv[4]); 
+    const double DY = atoi(argv[5]);
+    const double DZ = atoi(argv[6]);
+    
+    moab::Core mbcore;
+    moab::Interface& mbint = mbcore;
 
-  // *******************
-  // *   Attach Tags   *
-  // *******************
-  
-  // Create the tags:
-  //
-  // MB_TAG_DENSE allocates a tag to every mesh point
-  // Could be useful going forward to use MB_TAG_SPARSE, which only allocates
-  // tag memory as needed.
-  moab::Tag temp_tag;
-  double temp_default_value = 0.0;
-  rval = mbint.tag_get_handle("temperature", 1, MB_TYPE_DOUBLE, temp_tag, 
-                              MB_TAG_DENSE | moab::MB_TAG_CREAT, 
-			      &temp_default_value);
-  MB_CHK_SET_ERR(rval, "mbint.tag_get_handle(temperature)");
+    // moab::ScdInterface, structured mesh interface for MOAB
+    moab::ScdInterface *scdint;
+
+    // Pass structured mesh to MOAB for Error Queries
+    moab::ErrorCode rval = mbint.query_interface(scdint);
+    MB_CHK_SET_ERR(rval, "mbint.query_interface");
+    
+    // Create the mesh:
+    moab::ScdBox *scdbox = NULL;
+    rval = scdint->construct_box(moab::HomCoord(0,0,0), 
+                     moab::HomCoord(NI,NJ,NK),
+                     NULL, 
+                     0, 
+                     scdbox);
+    MB_CHK_SET_ERR(rval, "scdint->construct_box");
+
+    // set the vertices
+    for(unsigned i = 0; i < NI+1; i++) 
+      for(unsigned j = 0; j < NJ+1; j++)
+          for(unsigned k = 0; k < NK+1; k++) {
+        // First, get the entity handle:
+        moab::EntityHandle handle = scdbox->get_vertex(i,j,k);
+
+        // Compute the coordinate:
+        double coord[3] = {DX*i, DY*j, DZ*k};
+
+        // Change the coordinate of the vertex:
+        mbint.set_coords(&handle, 1, coord);
+      }
+
+    // Attach Tags
+    // MB_TAG_DENSE allocates a tag to every mesh point
+    // Could be useful going forward to use MB_TAG_SPARSE, which only allocates
+    // tag memory as needed.
+    moab::Tag temp_tag;
+    double temp_default_value = 0.0;
+    rval = mbint.tag_get_handle("track length", 1, MB_TYPE_DOUBLE, temp_tag, 
+                                MB_TAG_DENSE | moab::MB_TAG_CREAT, 
+                        &temp_default_value);
+    MB_CHK_SET_ERR(rval, "mbint.tag_get_handle(track length)");
+      
+    // Write the mesh object to file
+    const char* savefile = mesh_name.c_str();
+    rval = mbint.write_file(savefile);
+    MB_CHK_SET_ERR(rval, "write_file(struct_mesh.vtk)");
 
 
-
-  // ***************************
-  // *   Write Mesh to Files   *
-  // ***************************
-
-  // NOTE: Some visualization software (such as VisIt) may not
-  // interpret the velocity tag as a vector and you may not be able to
-  // plot it. But you should be able to plot the temperature on top of
-  // the mesh.
-
-  rval = mbint.write_file("moabuse4.vtk");
-  MB_CHK_SET_ERR(rval, "write_file(moabuse4.vtk)");
-
-
-  return 0;
+    return 0;
 }
